b42e921e630b73edc997777836b5a2388c23068e
fromkk <info@fromkk.info>
2016-08-20 21:20:02 +0900

    Initial Commit

---
+++ SlackTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlackTest
+//
+//  Created by Ueoka Kazuya on 2016/08/20.
+//  Copyright © 2016年 fromKK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
